3c01d11d252bc33adf8713287b2a75de2c926267
MMDz-123 <pencet552@gmail.com>
2025-10-24 17:31:22 +0700

    Modifikasi percobaan 1

--- jobsheet7/SiakadFor2.java
+++ jobsheet7/SiakadFor2.java
@@ -6,7 +6,8 @@ public class SiakadFor2 {
         Scanner sca=new Scanner(System.in);
         
         double nilai, trtinggi=0, trrendah=100;
-        int i;
+        int i, o=0;
+        int lls;
         
         for(i=1; i<=10; i++){
             System.out.println("Masukkan nilai mahasiswa ke-"+i);
@@ -17,9 +18,16 @@ public class SiakadFor2 {
             if(nilai<trrendah){
                 trrendah=nilai;
             }
+            if(nilai<60){
+                o++;
+            }
         }
+        lls=10-o;
         System.out.println("Nilai tertinggi="+trtinggi);
         System.out.println("Nilai terendah="+trrendah);
+        System.out.println("Mahasiswa yang tidak lulus="+o);
+        System.out.println("Mahasiswa yang lulus="+lls);
+
         
         sca.close();
     }
