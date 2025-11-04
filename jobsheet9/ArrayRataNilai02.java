64843f0fb5cbf33a2c3d9af008caf1eb57961916
Ahmad Ramadhan <pencet552@gmail.com>
2025-11-04 11:09:45 +0700

    Modifikasi percobaan 3

--- jobsheet9/ArrayRataNilai02.java
+++ jobsheet9/ArrayRataNilai02.java
@@ -4,20 +4,31 @@ import java.util.Scanner;
 public class ArrayRataNilai02 {
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
-        int nilaiMhs[]= new int[10];
-        double rata, tot=0;
+        double rata, ratatdk , totLls=0, totdklls=0;
+        int jml=0, jmlLLs=0, jmltdk=0;
+        
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        jml= scanner.nextInt();
+        int nilaiMhs[]= new int[jml];
 
         for(int i=0; i<nilaiMhs.length; i++){
             System.out.print("masukkan nilai mahasiswa ke-"+i+": ");
             nilaiMhs[i]= scanner.nextInt();
+            if(nilaiMhs[i]>70){
+                jmlLLs++;
+                totLls += nilaiMhs[i];
+            }
+            else{
+                jmltdk++;
+                totdklls+=nilaiMhs[i];
+            }
         }
 
-        for(int i=0; i<nilaiMhs.length; i++){
-            tot += nilaiMhs[i];
-        }
-
-        rata= tot/nilaiMhs.length;
-        System.out.println("Rata2 nilai mahasiswa = "+rata);
+        rata= totLls/jmlLLs;
+        ratatdk=totdklls/jmltdk;
+        
+        System.out.println("Rata2 nilai mahasiswa lulus = "+rata);
+        System.out.println("Rata2 nilai mahasiswa tidak lulus = "+ratatdk);
 
         scanner.close();
     }
